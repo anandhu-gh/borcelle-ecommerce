67e41d42a054d5cb4f437a2f3e17e13025624bf7
Anandhu <ajianandhu774@gmail.com>
2026-05-29 18:00:09 +0530

    Fix fixture order for PostgreSQL

--- build.sh
+++ build.sh
@@ -6,7 +6,6 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-# Load fixture data into PostgreSQL safely
 if [ -f data_backup.json ]; then
     python manage.py loaddata data_backup.json
 fi
