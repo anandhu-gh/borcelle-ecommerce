23977e1e1c527b1fe24c7009637c5c4838f1d452
Anandhu <ajianandhu774@gmail.com>
2026-05-29 10:51:10 +0530

    Standard setup to load ecommerce database data

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
 pip install -r requirements.txt
+
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# This officially loads your backup file data straight into the live database
+python manage.py loaddata ecommerce_data.json
