d9c6dce184a2bae34d718efe2e9f34396feb87bd
Anandhu <ajianandhu774@gmail.com>
2026-05-29 10:11:05 +0530

    Remove temporary json data file and restore standard build script

--- build.sh
+++ build.sh
@@ -6,6 +6,3 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-# This line injects your data into Render's database automatically
-python manage.py loaddata cloud_data.json
