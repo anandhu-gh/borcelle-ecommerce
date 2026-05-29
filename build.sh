6acb766163db2992441dc89cfea41671cfafc5d9
Anandhu <ajianandhu774@gmail.com>
2026-05-29 10:58:40 +0530

    Remove loaddata line to protect live user database

--- build.sh
+++ build.sh
@@ -6,6 +6,3 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-# This officially loads your backup file data straight into the live database
-python manage.py loaddata ecommerce_data.json
